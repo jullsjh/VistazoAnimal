b784077cc9c899dc5bc02085266bea548c6a5149
jullsjh <jh30.julls@gmail.com>
2022-02-23 14:17:14 +0100

    search animal

--- servidor/application.py
+++ servidor/application.py
@@ -1,34 +1,65 @@
 #importamos flask
 # from multiprocessing.connection import Client
-from flask import Flask, request, abort
+from flask import Flask, request, abort, jsonify
 from pymongo import MongoClient
 client = MongoClient('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB+Compass&ssl=false')
 
 #instanciamos la clase , creamos un nuevo objeto y le pasamos el __name__
-# application = Flask(__name__)
+application = Flask(__name__)
 
 # BBDD
 mongodb = client.VistazoAnimal
 
 
 # ----------------------------- ANIMALES -----------------------------
-# Buscar animal
-input_animal = input("Escribe nombre del animal que quieres buscar \n")
-filter = {'nombre': input_animal}
-# filter = {'nombre': 'Brown pelican'}
-# projection = {'id_habitat': 1}
+# Devuelve TODOS los animales
+@application.route("/all_animals", methods=['GET'])
+def all_animals():
+    # input_animal = 'bhjdawsvb' #input("Escribe nombre del animal que quieres buscar: \n")
+    filter = {}
+    projection = {
+        '_id': 0, 
+        'id_habitat': 0,
+        'id_especie': 0}
 
-animales = list(mongodb.animales.find(filter))
-for animal in animales:
-    print(animal)
+    animales = list(mongodb.animales.find(filter, projection))
+    for animal in animales:
+        print(animal)
+    
+    return jsonify(animales), 200
+
+
+
+
+# Buscar 1 animal
+input_animal = input("Escribe nombre del animal que quieres buscar: \n")
+
+@application.route("/", methods=['GET'])
+def search_animal():
+    filter = {'name': input_animal}
+    projection = {
+        '_id': 0, 
+        'id_habitat': 0,
+        'id_especie': 0}
+
+    animales = list(mongodb.animales.find(filter, projection))
+    for animal in animales:
+        print(animal)
+    
+    return jsonify(animales), 200
+    # return animales
 
 
 # ----------------------------- USUARIOS -----------------------------
 
 
+
+
+
+
 # 0 se muestra todo menos eso
 # muestra lo que tiene 1
-# projection es las columnas del select
+# projection es las columnas del select --> 0: no muestra, 1: muestra
 # filter es el where
 
 
@@ -36,8 +67,8 @@ for animal in animales:
 
 
 #Si el servidor se lleva a produccion se quita la parte de debug
-# if __name__ == '__main__':
-#     application.run(debug=True)
+if __name__ == '__main__':
+    application.run(debug=True)
 
 
 
